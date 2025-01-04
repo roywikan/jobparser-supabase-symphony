4b1c32d9d7009ddf879bb72d504cdb5af24c298f
wee <5243434+roywikan@users.noreply.github.com>
2025-01-04 13:51:03 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AuthPage />} />
+          <Route path="/auth" element={<AuthPage />} />
           <Route
             path="/"
             element={
